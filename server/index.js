f364db6c63ef618e6d56d39d14ea3c524f5d7a56
BigMike783 <45283126+BigMike783@users.noreply.github.com>
2019-05-30 12:13:44 -0600

    second commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express')
+const session = require('express-session'),
+      massive = require('massive'),
+      auth_ctrl = require('./controllers/auth_controller')
+
+const app = express()
+
+const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env
+app.use(express.json())
+app.use(
+    session({
+        resave: false,
+        saveUninitialized: false,
+        secret: SESSION_SECRET,
+        cookie: {
+            maxAge: 1000 * 60 *60
+        }
+    })
+)
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    console.log('data is flowing')
+})
+
+app.post('/auth/register', auth_ctrl.register)
+app.post('/auth/login', auth_ctrl.login)
+app.get('/auth/details', auth_ctrl.getDetails)
+
+
+app.listen(SERVER_PORT, () => console.log(`Doing the stuff on port ${SERVER_PORT}`))
